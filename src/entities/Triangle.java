0ad44f050ea51868edba61e22b1198a240758dc6
mlima3696 <mjuniorlima3@gmail.com>
2021-04-09 17:35:30 -0300

    Melhorando a classe Triangle

--- src/entities/Triangle.java
+++ src/entities/Triangle.java
@@ -5,24 +5,12 @@ public class Triangle {
 	public double a;
 	public double b;
 	public double c;
-	public double getA() {
-		return a;
-	}
-	public void setA(double a) {
-		this.a = a;
-	}
-	public double getB() {
-		return b;
-	}
-	public void setB(double b) {
-		this.b = b;
-	}
-	public double getC() {
-		return c;
-	}
-	public void setC(double c) {
-		this.c = c;
-	}
 	
+	public double  area() {
+		double p = (a+b+c)/2;
+		return Math.sqrt(p*(p-a)*(p-b)*(p-c));
+		//double result=Math.sqrt(p*(p-a)*(p-b)*(p-c));
+		//return result;
+	}
 	
 }
