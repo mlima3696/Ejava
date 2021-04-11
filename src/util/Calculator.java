fd3255ab22286f121ec040b7f04259e6a6f7f376
mlima3696 <mjuniorlima3@gmail.com>
2021-04-11 19:12:13 -0300

    Program2 terceira versão

--- src/util/Calculator.java
+++ src/util/Calculator.java
@@ -3,13 +3,13 @@ package util;
 
 public class Calculator {
 	
-	public  final double PI=3.14159; // Final ele não pode mudar (O valor vai ser constante)
+	public static final double PI=3.14159; // Final ele não pode mudar (O valor vai ser constante)
 	
-	public  double circumference(double radius) {
+	public static double circumference(double radius) {
 		return 2*PI*radius;
 	}
 	
-	public  double volume(double radius) {
+	public static double volume(double radius) {
 		return  (4*PI*(radius*radius*radius))/3;
 	}
 
