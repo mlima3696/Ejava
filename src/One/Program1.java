9372a283813818bbbec85a91caaef7da36e9fd6b
mlima3696 <mjuniorlima3@gmail.com>
2021-04-12 01:21:00 -0300

    Sobrecarga

--- src/One/Program1.java
+++ src/One/Program1.java
@@ -17,16 +17,14 @@ public class Program1 {
 		String name=sc.nextLine();
 		System.out.print("Price: ");
 		double price=sc.nextDouble();
-		System.out.print("Quantity in stock: ");
-		int quantity=sc.nextInt();
 		
-		Product product = new Product(name, price, quantity);
+		Product product = new Product(name, price);
 
 		System.out.println();
 		System.out.print("Product data: " + product);
 		System.out.println();
 		System.out.print("Enter the number of products to be added in stock: ");
-		quantity=sc.nextInt();
+		int quantity=sc.nextInt();
 		product.addProducts(quantity);
 		System.out.println();
 		System.out.print("Updated data: " + product);
